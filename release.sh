760eb02b5b07b0e9dbe1105366149e139cd38b4d
Marcel Hecko <maco@blava.net>
2014-02-02 10:40:05 -0500

    migrated to nodejs

--- release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mysqldump -d -u root --skip-add-drop-table -p marcel.env > server/install/mysql.schema
--- server/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-date_default_timezone_set("Europe/London");
-require('config_local.php');
-require('db.php');
-?>
