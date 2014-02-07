153cd68396aa3b1f3389fb01d3672805cd1836ed
Marcel Hecko <maco@blava.net>
2014-02-07 22:45:36 +0000

    wind vane simulation added and removed arbitrary calculations

--- rpi_client/serial_comms.py
+++ rpi_client/serial_comms.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-# 0 - test
+# 0 - anemometer - reset counter
 # 1 - anemometer
 # 2 - light sensor
 # 3 - temp
+# 4 - wind vane
 
 import serial
 import time
@@ -26,9 +27,15 @@ ser.write("1")
 out = ser.readline()
 count = re.split(':|\\n', out)[2]
 freq = float(count) / float(sleep) / 2.0
-utils.send_to_cloud("hacklab", "wind_freq", freq)
+mps = float(freq) / 0.777  # 0.777Hz per m/s nominal - from vectorinstruments web site
+utils.send_to_cloud("hacklab", "wind_speed", mps)
 
 ser.write("3")
 out = ser.readline()
 light = re.split(':|\\n', out)[2]
 utils.send_to_cloud("hacklab", "temp", light)
+
+ser.write("4")
+out = ser.readline()
+wv = re.split(':|\\n', out)[2]
+utils.send_to_cloud("hacklab", "wind_direction", wv)
