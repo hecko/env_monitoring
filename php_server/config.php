6075e04a59c3424f77bf6d90fdaf56585d7fed2f
Marcel Hecko <maco@blava.net>
2014-02-10 16:17:08 +0100

    added php version of the interface

--- php_server/config.php
+++ php_server/config.php
@@ -4,4 +4,6 @@ $app_location = 'Edinburgh';
 $db_user      = 'iboredco_mac';
 $db_pass      = 'testing123';
 $db_db        = 'iboredco_weather';
+$title        = 'iBored Weather';
+$token        = 'hacklab'; // default token
 ?>
--- php_server/db.php
+++ php_server/db.php
@@ -1,5 +1,4 @@
 <?php
-include('config.php');
 $con=mysqli_connect("localhost",$db_user,$db_pass,$db_db);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
