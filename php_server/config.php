5fba7abeec55452a23e43c6494ed367f2c368c27
Marcel Hecko <maco@blava.net>
2014-02-10 13:47:35 +0000

    adding php version of the server

---
+++ php_server/config.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("Europe/London");
+$app_location = 'Edinburgh';
+$db_user      = 'iboredco_mac';
+$db_pass      = 'testing123';
+$db_db        = 'iboredco_weather';
+?>
---
+++ php_server/db.php
@@ -0,0 +1,7 @@
+<?php
+include('config.php');
+$con=mysqli_connect("localhost",$db_user,$db_pass,$db_db);
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
