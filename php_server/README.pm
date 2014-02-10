44fd5f24ed3b2ae7642485536fc92178bbe31223
Marcel Hecko <maco@blava.net>
2014-02-10 16:18:55 +0100

    fix

--- php_server/README.pm
+++ php_server/README.pm
@@ -5,7 +5,7 @@
 - allow access for the user to the database
 - change the details in config.php
 - create database structure (e.g. create tebles):
-  mysqldump -h localhost -p -u iboredco_mac iboredco_weather < mysql_struct.sql
+  mysql -h localhost -p -u iboredco_mac iboredco_weather < mysql_struct.sql
 - load index.php in browser
 
 = Notes =
