86ab58933311b39c539fde3dc93c90b8eee1a710
Marcel Hecko <maco@blava.net>
2014-02-19 18:20:46 +0000

    more debug code

--- arduino_firmware/ethernet_test/ethernet_test.ino
+++ arduino_firmware/ethernet_test/ethernet_test.ino
@@ -9,13 +9,12 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-
 char token[] = "hacklab";
 char server[] = "knotsup.ibored.com.au";
 
-// IPAddress ip(10,0,0,38);
-// IPAddress myDns(10,0,0,138);
-// IPAddress myGW(10,0,0,138);
+//IPAddress ip(10,0,0,38);
+//IPAddress myDns(10,0,0,138);
+//IPAddress myGW(10,0,0,138);
 
 IPAddress ip(10,38,38,38);
 IPAddress myDns(10,38,38,1);
@@ -30,38 +29,61 @@ void setup() {
   Serial.begin(9600);
   // give the ethernet module time to boot up:
 
-  Ethernet.begin(mac, ip, myDns, myGW);
+  if (Ethernet.begin(mac) == 0) {
+    // if we dont get IP from DHCP, assign the address manually
+    Serial.println("Did not get IP from DHCP - setting a static one");
+    Ethernet.begin(mac, ip, myDns, myGW);
+  }
   W5100.setRetransmissionTime(0x1388);
   W5100.setRetransmissionCount(3);
 
   dht.begin();  // humidity and temperature
 
-  delay(1000);
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
+  Serial.println("Will start sending data in 10 seconds");
+  delay(1000);
 }
 
 void loop() {
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+   if (client.available()) {
+      char c = client.read();
+      Serial.print(c);
   }
+
+  client.stop();
+
+  delay(10000);
+
   float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float t = dht.readTemperature(false);
   if (isnan(t) || isnan(h)) {
-    Serial.println("Failed to read from DHT");
+    Serial.println("Failed to read from DHT22 sensor");
   } 
   else {
-    send_data('humidity',h);
-    send_data('temp'    ,t);
+    char t_char[10];
+    char h_char[10];
+    sprintf(t_char,"%f",t);
+    sprintf(h_char,"%f",h);
+    send_data("temp"    ,t_char);
+    send_data("humidity",h_char);
   }
+  
+  send_data("beat", "1"); // test beat
+
+    Serial.println("------------");
 }
 
-void send_data(char key, float val) {
+void send_data(char key[], char val[]) {
+  Serial.print("Trying to connect to server ");
+  Serial.println(server);
   if (client.connect(server, 80)) {
-    //    float val = 100.0 / 1024.0 * analogRead(TEST_PIN);
+    Serial.print("Sending key: ");
+    Serial.print(key);
+    Serial.print(" value: ");
     Serial.print(val);
-    Serial.println(" <- sending...");
+    Serial.print(" to server ");
+    Serial.println(server);
     client.print("GET /put_get.php?key=");
     client.print(key);
     client.print("&val=");
@@ -75,10 +97,12 @@ void send_data(char key, float val) {
   } 
   else {
     Serial.print("Unable to connect to ");
-    Serial.println(server); 
-    client.stop();
+    Serial.println(server);
+    client.stop(); 
   }
 }
+
+
 
 
 
