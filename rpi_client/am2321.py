11ad59e23f6c8fe72c4c3aec6d8e5d1ac8789447
root <root@raspberrypi.(none)>
2014-02-28 18:47:26 +0000

    funguje pekne

--- rpi_client/am2321.py
+++ rpi_client/am2321.py
@@ -8,25 +8,40 @@
 
 import lib.utils as utils
 import subprocess
+import sys
+from tendo import singleton
+import argparse
+
+me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
+
+parser = argparse.ArgumentParser(description='Gets humidity and temperature data from AM2321 and sends to server.')
+
+parser.add_argument('-s', '--send',    action="store_true", help='Send to server, otherwise just test reading the sensors.')
+parser.add_argument('-v', '--verbose', action="store_true", help='Print out more info.')
+
+args = parser.parse_args()
+
 
 def read_sensor(pin):
-    text = subprocess.check_output(["./Adafruit_DHT_Driver/Adafruit_DHT", "2302", str(pin)])
+    out = {}
+    text = subprocess.check_output(["../external/Adafruit-Raspberry-Pi-Python-Code/Adafruit_DHT_Driver/Adafruit_DHT", "2302", str(pin)])
 
     # split the two lines
     lines = text.split("\n")
 
-    print lines
-    return
-
     # make sure the crc is valid
-    if lines[0].find("YES") > 0:
-        # get the 9th (10th) chunk of text and lose the t= bit
-        temp = float((lines[1].split(" ")[9])[2:])
-        # add a decimal point
-        temp /= 1000
-        return temp
+    if len(lines) == 4:
+        out['temp'] = float(lines[2].split()[2])
+        out['humi'] = float(lines[2].split()[6])
+        return out
+    else:
+        sys.exit("unable to read sensor data")
 
-now = read_sensor(4)
+now = read_sensor(4) # pin to which the sensor is connected to
 
-#utils.send_to_cloud("temp_hum", now[temp])
-#utils.send_to_cloud("humi_hum", now[humi])
+if args.verbose:
+    print now
+
+if args.send:
+    utils.send_to_cloud("temp", now['temp'])
+    utils.send_to_cloud("humidity", now['humi'])
