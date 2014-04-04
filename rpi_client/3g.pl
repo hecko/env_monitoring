efff38e03e61f80dd81743eaabbf0eda60b1416c
root <root@raspberrypi.(none)>
2014-04-04 17:02:44 +0100

    newlines

--- rpi_client/3g.pl
+++ rpi_client/3g.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 
 if (status() == 0) {
-  print "We are NOT connected - try to connect now!";
+  print "We are NOT connected - try to connect now!\n";
   my $connect = `/opt/env_monitoring/external/sakis3g-source/build/sakis3gz connect APN="payandgo.o2.co.uk"`;
-  print $connect;
+  print $connect . "\n";
 } else {
-  print "We are connected :)";
+  print "We are connected :)\n";
 }
 
 sub status {
@@ -13,10 +13,10 @@ sub status {
   my $status_code = $? >> 8;
   print "Status code: " . $status_code . "\n";
   if ($status_code != 0) {
-    print "Not connected!";
+    print "Not connected!\n";
     return 0;
   } else {
-    print "Yes, connected!";
+    print "Yes, connected!\n";
     return 1;
   }
 }
