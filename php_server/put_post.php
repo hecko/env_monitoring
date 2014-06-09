fba4e58b0d7a14bcc771600d2482a9c94d8a8c6e
Marcel Hecko <maco@blava.net>
2014-06-09 17:01:39 +0200

    remove removed code

--- php_server/put_post.php
+++ php_server/put_post.php
@@ -12,25 +12,13 @@ $json = preg_replace('/\'/', '"', $json);
 
 $d = json_decode($json, true);
 
-//if (!isset($d['token'])) {
-//    //die("token not set\n");
-//    header("HTTP/1.1 200 OK");
-//}
-
-//if (!isset($d['data'][0]['timestamp'])) {
-//    $d['data'][0]['timestamp'] = time();
-//}
-
 foreach ($d['data'] as $i) {
     if (!$device_id = get_device_id($con, 'serial', $i['serial'])) {
         header("X-cube_cx: Device with that serial not found!");
         header('HTTP/1.1 400');
         return;
     }
-    if ($i['key'] == 1) {
-        $i['key'] = 'temp';
-        $i['val'] = $i['val'] / 100;
-    };
+
     $sql = "INSERT INTO data (`device_id`, `key`, `val`, `unit`, `time`) VALUES ('"
         .mysqli_real_escape_string($con,$device_id)."', '"
         .mysqli_real_escape_string($con,$i['key'])."', "
